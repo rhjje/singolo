9e0a33dcb004dc4f5b8e5f90cdc7aac9e4bac0b1
rhjje <numelen@gmail.com>
2020-12-30 01:15:42 +0300

    init: start singolo

---
+++ src/js/images.js
@@ -0,0 +1,27 @@
+// import sliderContent from '../assets/images/sliderContent.png';
+// import servicesItem1 from '../assets/icons/services-item-1.svg';
+// import servicesItem2 from '../assets/icons/services-item-2.svg';
+// import servicesItem3 from '../assets/icons/services-item-3.svg';
+// import servicesItem4 from '../assets/icons/services-item-4.svg';
+// import servicesItem5 from '../assets/icons/services-item-5.svg';
+// import servicesItem6 from '../assets/icons/services-item-6.svg';
+
+// const firstSlide = document.querySelector('.first-slide');
+// firstSlide.src = sliderContent;
+
+// const service1 = document.querySelector('.service-1');
+// const service2 = document.querySelector('.service-2');
+// const service3 = document.querySelector('.service-3');
+// const service4 = document.querySelector('.service-4');
+// const service5 = document.querySelector('.service-5');
+// const service6 = document.querySelector('.service-6');
+// service1.src = servicesItem1;
+// service2.src = servicesItem2;
+// service3.src = servicesItem3;
+// service4.src = servicesItem4;
+// service5.src = servicesItem5;
+// service6.src = servicesItem6;
+
+document.querySelector('.click').addEventListener('click', () => {
+  document.querySelector('.click').src = './images/Slider.png';
+});
